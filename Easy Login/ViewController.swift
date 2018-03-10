196b22124072624a4475054932f38b5040fee22b
test <asdfg@mail.com>
2018-03-10 13:25:33 +0700

    check password ok

--- Easy Login/ViewController.swift
+++ Easy Login/ViewController.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userString: String = ""
+    var passwordString: String = ""
+    var resultString = ""
+    
+    let officeDict = ["user1":"1111","user2":"2222","user3": "3333"]
+    let dialogString = ["","User False","Password False", "Welcome"]
 //    Explicit
     @IBOutlet weak var showLabel: UILabel!
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBAction func loginButton(_ sender: Any) {
+//        Get value from text field
+        userString = userTextField.text!
+        passwordString = passwordTextField.text!
         
-    }
+        print("User ==> \(userString)")
+        print("Password ==> \(passwordTextField)")
+        resultString = officeDict[userString]!
+        print("result ==> \(resultString)")
+        if(resultString == passwordString){
+//            Auther True
+            showLabel.text = dialogString[3]
+        }else{
+//            password false
+            showLabel.text = dialogString[2]
+        }
+        
+    }//loginButton
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
